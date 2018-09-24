7bdd732f5c33bd56abae979db3d694a0324488a4
Manoj Chalode <chalodem@gmail.com>
2018-09-24 12:10:35 +0100

    master - added debugger.

--- test/index-page.test.js
+++ test/index-page.test.js
@@ -1,9 +1,28 @@
-var assert = require('assert');
+const assert = require('assert');
 
 describe('webdriver.io page', function () {
     it('should have the right title - the fancy generator way', function () {
-        browser.url('http://webdriver.io');
-        var title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
+        this.timeout(10000);
+        browser.url('/login');
+        const title = browser.getTitle();
+        assert.equal(title, 'Login | PREPROD | Localstars Advance');
+    });
+
+    it.only('should login with valid username and password', async function (done) {
+        this.timeout(40000);
+        browser.url('/login');
+        await browser.pause(4000);
+
+        const username = $('#email');
+        await username.setValue("admin");
+
+        const password = $('#password');
+        await password.setValue("foreveradvancing");
+
+        await browser.submitForm('#login > form');
+
+        setTimeout(() => {
+            done();
+        }, 4000);
     });
 });
